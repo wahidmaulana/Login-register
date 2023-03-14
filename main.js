2c674bb428e8d9a9ec0709c23ee16de43ef3779f
M. Wahid Maulana <wahidmaulana236@gmail.com>
2023-03-15 06:42:03 +0700

    add and edit login-register

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//* hamburger menu
+
 const hamburger = document.querySelector(".hamburger");
 const navLink = document.querySelector(".nav-link");
 const links = document.querySelectorAll(".nav-link li");
@@ -7,4 +9,28 @@ hamburger.addEventListener("click", () => {
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
+});
+
+//! input events
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const loginButton = document.querySelector(".btn-login");
+const closeButton = document.querySelector(".close-btn");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+loginButton.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+closeButton.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
